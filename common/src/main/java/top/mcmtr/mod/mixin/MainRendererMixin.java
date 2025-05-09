9f6b7655bfd8370f02fd0b40dbcb40757dc2acec
Kenny-Hui <kenny.mh.hui@outlook.com>
2025-05-09 16:14:12 +0800

    Fix MSD Catenary not rendering

--- common/src/main/java/top/mcmtr/mod/mixin/MainRendererMixin.java
+++ common/src/main/java/top/mcmtr/mod/mixin/MainRendererMixin.java
@@ -32,14 +32,14 @@ import java.util.UUID;
 
 @Mixin(MainRenderer.class)
 public class MainRendererMixin {
-    @Inject(method = "Lmtr/render/MainRenderer;renderAbsolute(Lnet/minecraft/client/Minecraft;Lnet/minecraft/client/player/LocalPlayer;Lnet/minecraft/world/level/Level;FLcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/renderer/MultiBufferSource;)V", at = @At(value = "INVOKE", target = "Ljava/util/Map;forEach(Ljava/util/function/BiConsumer;)V", ordinal = 0))
+    @Inject(method = "Lmtr/render/MainRenderer;renderRelative(Lnet/minecraft/client/Minecraft;Lnet/minecraft/client/player/LocalPlayer;Lnet/minecraft/world/level/Level;FLcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/renderer/MultiBufferSource;)V", at = @At(value = "INVOKE", target = "Lmtr/render/MainRenderer;renderTrains(Lnet/minecraft/world/level/Level;FLcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/renderer/MultiBufferSource;)V"))
     private static void renderCatenary(Minecraft minecraft, LocalPlayer player, Level level, float tickDelta, PoseStack matrices, MultiBufferSource vertexConsumers, CallbackInfo ci) {
-        final int renderDistanceChunks2 = UtilitiesClient.getRenderDistance();
-        final int maxCatenaryDistance2 = renderDistanceChunks2 * 16;
+        final int renderDistanceChunks = UtilitiesClient.getRenderDistance();
+        final int maxCatenaryDistance = renderDistanceChunks * 16;
         final Map<UUID, Boolean> renderedCatenaryMap = new HashMap<>();
         matrices.pushPose();
         MSDClientData.CATENARIES.forEach((startPos, catenaryMap) -> catenaryMap.forEach((endPos, catenary) -> {
-            if (!Util.isBetween(player.getX(), startPos.getX(), endPos.getX(), maxCatenaryDistance2) || !Util.isBetween(player.getZ(), startPos.getZ(), endPos.getZ(), maxCatenaryDistance2)) {
+            if (!Util.isBetween(player.getX(), startPos.getX(), endPos.getX(), maxCatenaryDistance) || !Util.isBetween(player.getZ(), startPos.getZ(), endPos.getZ(), maxCatenaryDistance)) {
                 return;
             }
             final UUID catenaryProduct = PathData.getRailProduct(startPos, endPos);
@@ -52,7 +52,7 @@ public class MainRendererMixin {
             }
             if (catenary.catenaryType == CatenaryType.ELECTRIC) {
                 catenary.render((x1, y1, z1, x2, y2, z2, count, i, base, sinX, sinZ, increment) ->
-                        IDrawing.drawLine(matrices, vertexConsumers, (float) x1, (float) y1 + 0.5F, (float) z1, (float) x2, (float) y2 + 0.5F, (float) z2, 0, 0, 0)
+                    IDrawing.drawLine(matrices, vertexConsumers, (float) x1, (float) y1 + 0.5F, (float) z1, (float) x2, (float) y2 + 0.5F, (float) z2, 0, 0, 0)
                 );
             } else if (catenary.catenaryType == CatenaryType.RIGID_SOFT_CATENARY) {
                 renderRigidSoftCatenaryStandard(catenary);
@@ -64,7 +64,7 @@ public class MainRendererMixin {
         matrices.pushPose();
         final Map<UUID, Boolean> renderedRigidCatenaryMap = new HashMap<>();
         MSDClientData.RIGID_CATENARIES.forEach((startPos, rigidCatenaryMap) -> rigidCatenaryMap.forEach((endPos, rigidCatenary) -> {
-            if (!Util.isBetween(player.getX(), startPos.getX(), endPos.getX(), maxCatenaryDistance2) || !Util.isBetween(player.getZ(), startPos.getZ(), endPos.getZ(), maxCatenaryDistance2)) {
+            if (!Util.isBetween(player.getX(), startPos.getX(), endPos.getX(), maxCatenaryDistance) || !Util.isBetween(player.getZ(), startPos.getZ(), endPos.getZ(), maxCatenaryDistance)) {
                 return;
             }
             final UUID rigidCatenaryProduct = PathData.getRailProduct(startPos, endPos);
@@ -81,7 +81,7 @@ public class MainRendererMixin {
         matrices.pushPose();
         final Map<UUID, Boolean> renderedTransCatenaryMap = new HashMap<>();
         MSDClientData.TRANS_CATENARIES.forEach((startPos, catenaryMap) -> catenaryMap.forEach((endPos, catenary) -> {
-            if (!Util.isBetween(player.getX(), startPos.getX(), endPos.getX(), maxCatenaryDistance2) || !Util.isBetween(player.getZ(), startPos.getZ(), endPos.getZ(), maxCatenaryDistance2)) {
+            if (!Util.isBetween(player.getX(), startPos.getX(), endPos.getX(), maxCatenaryDistance) || !Util.isBetween(player.getZ(), startPos.getZ(), endPos.getZ(), maxCatenaryDistance)) {
                 return;
             }
             final UUID catenaryProduct = PathData.getRailProduct(startPos, endPos);
@@ -150,13 +150,35 @@ public class MainRendererMixin {
 
     private static void renderRigidCatenaryStandard(Level world, RigidCatenary rigidCatenary, String texture) {
         final int maxCatenaryDistance = UtilitiesClient.getRenderDistance() * 16;
-        rigidCatenary.render((x1, z1, x2, z2, x3, z3, x4, z4, x5, z5, x6, z6, x7, z7, x8, z8, y1, y2) -> {
-            final BlockPos pos3 = BlockUtil.newBlockPos(x1, y1, z1);
+        rigidCatenary.render((absx1, absz1, absx2, absz2, absx3, absz3, absx4, absz4, absx5, absz5, absx6, absz6, absx7, absz7, absx8, absz8, absy1, absy2) -> {
+            final BlockPos pos3 = BlockUtil.newBlockPos(absx1, absy1, absz1);
             if (MainRenderer.shouldNotRender(pos3, maxCatenaryDistance, null)) {
                 return;
             }
+
+            final double x1 = 0;
+            final double z1 = 0;
+            final double x2 = absx2 - absx1;
+            final double z2 = absz2 - absz1;
+            final double x3 = absx3 - absx1;
+            final double z3 = absz3 - absz1;
+            final double x4 = absx4 - absx1;
+            final double z4 = absz4 - absz1;
+            final double x5 = absx5 - absx1;
+            final double z5 = absz5 - absz1;
+            final double x6 = absx6 - absx1;
+            final double z6 = absz6 - absz1;
+            final double x7 = absx7 - absx1;
+            final double z7 = absz7 - absz1;
+            final double x8 = absx8 - absx1;
+            final double z8 = absz8 - absz1;
+            final double y1 = 0;
+            final double y2 = absy2 - absy1;
+
             final int light2 = LightTexture.pack(world.getBrightness(LightLayer.BLOCK, pos3), world.getBrightness(LightLayer.SKY, pos3));
             MainRenderer.scheduleRender(ResourceLocation.parse(texture), false, MainRenderer.QueuedRenderLayer.EXTERIOR, (matrices, vertexConsumer) -> {
+                matrices.pushPose();
+                MainRenderer.transformRelativeToCamera(matrices, absx1, absy1, absz1);
                 IDrawing.drawTexture(matrices, vertexConsumer, (float) x1, (float) y1, (float) z1, (float) x2, (float) y1, (float) z2, (float) x3, (float) y2, (float) z3, (float) x4, (float) y2, (float) z4, 0.0F, 0.0F, 1.0F, 0.03125F, Direction.UP, -1, 0);
                 IDrawing.drawTexture(matrices, vertexConsumer, (float) x3, (float) y2, (float) z3, (float) x2, (float) y1, (float) z2, (float) x1, (float) y1, (float) z1, (float) x4, (float) y2, (float) z4, 0.0F, 0.03125F, 1.0F, 0.0F, Direction.UP, -1, 0);
                 IDrawing.drawTexture(matrices, vertexConsumer, (float) x1, (float) y1, (float) z1, (float) x5, (float) y1 + 0.125F, (float) z5, (float) x8, (float) y2 + 0.125F, (float) z8, (float) x4, (float) y2, (float) z4, 0.0F, 0.5F, 1.0F, 1.0F, Direction.UP, -1, light2);
@@ -165,35 +187,54 @@ public class MainRendererMixin {
                 IDrawing.drawTexture(matrices, vertexConsumer, (float) x7, (float) y2 + 0.125F, (float) z7, (float) x6, (float) y1 + 0.125F, (float) z6, (float) x2, (float) y1, (float) z2, (float) x3, (float) y2, (float) z3, 0.0F, 1.0F, 1.0F, 0.5F, Direction.UP, -1, light2);
                 IDrawing.drawTexture(matrices, vertexConsumer, (float) x5, (float) y1 + 0.125F, (float) z5, (float) x6, (float) y1 + 0.125F, (float) z6, (float) x7, (float) y2 + 0.125F, (float) z7, (float) x8, (float) y2 + 0.125F, (float) z8, 0.0F, 0.5F, 1.0F, 1.0F, Direction.UP, -1, light2);
                 IDrawing.drawTexture(matrices, vertexConsumer, (float) x7, (float) y2 + 0.125F, (float) z7, (float) x6, (float) y1 + 0.125F, (float) z6, (float) x5, (float) y1 + 0.125F, (float) z5, (float) x8, (float) y2 + 0.125F, (float) z8, 0.0F, 1.0F, 1.0F, 0.5F, Direction.UP, -1, light2);
-
+                matrices.popPose();
             });
         });
     }
 
     private static void renderRigidSoftCatenaryStandard(Catenary catenary, String texture) {
         final int maxCatenaryDistance = UtilitiesClient.getRenderDistance() * 16;
-        catenary.render((x1, y1, z1, x2, y2, z2, count, i, base, sinX, sinZ, increment) -> {
-            final BlockPos pos3 = BlockUtil.newBlockPos(x1, y1, z1);
+        catenary.render((absX1, absY1, absZ1, absX2, absY2, absZ2, count, i, base, sinX, sinZ, increment) -> {
+            final BlockPos pos3 = BlockUtil.newBlockPos(absX1, absY1, absZ1);
             if (MainRenderer.shouldNotRender(pos3, maxCatenaryDistance, null)) {
                 return;
             }
+
+            final double x1 = 0;
+            final double y1 = 0;
+            final double z1 = 0;
+            final double x2 = absX2 - absX1;
+            final double y2 = absY2 - absY1;
+            final double z2 = absZ2 - absZ1;
+
             MainRenderer.scheduleRender(ResourceLocation.parse(texture), false, MainRenderer.QueuedRenderLayer.EXTERIOR, (matrices, vertexConsumer) -> {
+                matrices.pushPose();
+                MainRenderer.transformRelativeToCamera(matrices, absX1, absY1, absZ1);
                 IDrawing.drawTexture(matrices, vertexConsumer, (float) (x1 - sinX), (float) y1, (float) (z1 + sinZ), (float) (x2 - sinX), (float) y2, (float) (z2 + sinZ), (float) (x2 + sinX), (float) y2, (float) (z2 - sinZ), (float) (x1 + sinX), (float) y1, (float) (z1 - sinZ), 0.0F, 0.0F, 1.0F, 0.03125F, Direction.UP, -1, 0);
                 IDrawing.drawTexture(matrices, vertexConsumer, (float) (x2 - sinX), (float) y2, (float) (z2 + sinZ), (float) (x1 - sinX), (float) y1, (float) (z1 + sinZ), (float) (x1 + sinX), (float) y1, (float) (z1 - sinZ), (float) (x2 + sinX), (float) y2, (float) (z2 - sinZ), 0.0F, 0.03125F, 1.0F, 0.0F, Direction.UP, -1, 0);
                 IDrawing.drawTexture(matrices, vertexConsumer, (float) x1, (float) y1 + 0.03125F, (float) z1, (float) x2, (float) y2 + 0.03125F, (float) z2, (float) x2, (float) y2, (float) z2, (float) x1, (float) y1, (float) z1, 0.0F, 0.0F, 1.0F, 0.03125F, Direction.UP, -1, 0);
                 IDrawing.drawTexture(matrices, vertexConsumer, (float) x2, (float) y2 + 0.03125F, (float) z2, (float) x1, (float) y1 + 0.03125F, (float) z1, (float) x1, (float) y1, (float) z1, (float) x2, (float) y2, (float) z2, 0.0F, 0.03125F, 1.0F, 0.0F, Direction.UP, -1, 0);
+                matrices.popPose();
             });
         });
     }
 
     private static void renderTransCatenaryStandard(TransCatenary catenary, String texture) {
         final int maxCatenaryDistance = UtilitiesClient.getRenderDistance() * 16;
-        catenary.render((x1, y1, z1, x2, y2, z2, count, i, base, sinX, sinZ, increment) -> {
-            final BlockPos pos3 = BlockUtil.newBlockPos(x1, y1, z1);
+        catenary.render((absX1, absY1, absZ1, absX2, absY2, absZ2, count, i, base, sinX, sinZ, increment) -> {
+            final BlockPos pos3 = BlockUtil.newBlockPos(absX1, absY1, absZ1);
             if (MainRenderer.shouldNotRender(pos3, maxCatenaryDistance, null)) {
                 return;
             }
+            final double x1 = 0;
+            final double y1 = 0;
+            final double z1 = 0;
+            final double x2 = absX2 - absX1;
+            final double y2 = absY2 - absY1;
+            final double z2 = absZ2 - absZ1;
             MainRenderer.scheduleRender(ResourceLocation.parse(texture), false, MainRenderer.QueuedRenderLayer.EXTERIOR, (matrices, vertexConsumer) -> {
+                matrices.pushPose();
+                MainRenderer.transformRelativeToCamera(matrices, absX1, absY1, absZ1);
                 if (count < 8) {
                     IDrawing.drawTexture(matrices, vertexConsumer, (float) x1, (float) y1 + 0.65F + (float) base, (float) z1, (float) x2, (float) y2 + 0.65F + (float) base, (float) z2, (float) x2, (float) y2, (float) z2, (float) x1, (float) y1, (float) z1, 0.0F, 0.0F, 1.0F, 1.0F, Direction.UP, -1, 0);
                     IDrawing.drawTexture(matrices, vertexConsumer, (float) x2, (float) y2 + 0.65F + (float) base, (float) z2, (float) x1, (float) y1 + 0.65F + (float) base, (float) z1, (float) x1, (float) y1, (float) z1, (float) x2, (float) y2, (float) z2, 0.0F, 1.0F, 1.0F, 0.0F, Direction.UP, -1, 0);
@@ -211,6 +252,7 @@ public class MainRendererMixin {
                 }
                 IDrawing.drawTexture(matrices, vertexConsumer, (float) (x1 - sinX), (float) y1, (float) (z1 + sinZ), (float) (x2 - sinX), (float) y2, (float) (z2 + sinZ), (float) (x2 + sinX), (float) y2, (float) (z2 - sinZ), (float) (x1 + sinX), (float) y1, (float) (z1 - sinZ), 0.0F, 0.0F, 1.0F, 0.03125F, Direction.UP, -1, 0);
                 IDrawing.drawTexture(matrices, vertexConsumer, (float) (x2 - sinX), (float) y2, (float) (z2 + sinZ), (float) (x1 - sinX), (float) y1, (float) (z1 + sinZ), (float) (x1 + sinX), (float) y1, (float) (z1 - sinZ), (float) (x2 + sinX), (float) y2, (float) (z2 - sinZ), 0.0F, 0.03125F, 1.0F, 0.0F, Direction.UP, -1, 0);
+                matrices.popPose();
             });
         });
     }
